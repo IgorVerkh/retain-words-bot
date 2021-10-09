d95c0817f6b8a820286f17f9756d37ac5c3d7ee9
Igor <igor.verkh@gmail.com>
2021-10-09 20:28:40 +0500

    Bind to port just because

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,14 +3,17 @@ import com.github.kotlintelegrambot.dispatch
 import com.github.kotlintelegrambot.dispatcher.command
 import commands.*
 import org.litote.kmongo.KMongo
+import java.net.ServerSocket
 import java.util.Timer
 
 object MyBotConfig {
-    val API_TOKEN: String = System.getenv("API_TOKEN")?: error("No api token was passed")
+    val API_TOKEN: String = System.getenv("API_TOKEN") ?: error("No api token was passed")
     val DATABASE_URI: String = System.getenv("DATABASE_URI") ?: error("No database address was passed")
+    val PORT: Int = System.getenv("PORT")?.toInt() ?: 8080
 }
 
 fun main() {
+    val server = ServerSocket(MyBotConfig.PORT)
     val dbClient = KMongo.createClient(MyBotConfig.DATABASE_URI)
     val database = dbClient.getDatabase(DBConfig.DB_NAME)
     val timer = Timer()
